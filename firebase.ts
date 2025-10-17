079e3d8969f992efc227beb7455f449d5b33dd64
jasonankeodendaal <123748512+jasonankeodendaal@users.noreply.github.com>
2025-10-17 14:01:25 +0200

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,12 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCJPiGbBs-T9Mj_wcvby-WtThkoZbDqUzU",
   authDomain: "inked-605f2.firebaseapp.com",
@@ -15,6 +9,3 @@ const firebaseConfig = {
   messagingSenderId: "738054676293",
   appId: "1:738054676293:web:6f64deb80ef981d67a282c"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
